a5060ee7524a181e63b51a8448e7ef0f96e4c8d9
Fernando Mendoza R <fernandoza@gmail.com>
2019-04-15 15:12:37 -0500

    feat: add examples for servlets lecture

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        response.setContentType("text/html");
+        PrintWriter output = response.getWriter();
+        output.println("<h1>Hello,  "+name+"!</h1><hr>");
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String email = request.getParameter("email");
+        PrintWriter output = response.getWriter();
+        output.println("I saved your email,  "+email+"");
+    }
+
 }
+
